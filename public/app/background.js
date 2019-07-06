66fb251b9db60da33e9455698cbfa6b00328f770
Muhzi4u <muhsinkeramam@gmail.com>
2019-07-06 15:28:29 +0530

    :sparkles: complete a minimal ui example

--- public/app/background.js
+++ public/app/background.js
@@ -1,43 +1,47 @@
 const blinkerSettings = {
-  durationTime: 20000,
+  durationTime: 40000,
   message: 'Give your eyes a break and reduce your eye strain',
   reminderDuration: 40000
 };
 // eslint-disable-next-line
 chrome.storage.sync.set(blinkerSettings, function() {});
 // eslint-disable-next-line
-chrome.storage.sync.get(
-  ['durationTime', 'message', 'reminderDuration'],
-  function(items) {
-    console.log('durationTime', items);
-  }
-);
+chrome.storage.sync.get(['durationTime'], function(items) {
+  console.log('durationTime', items);
+});
 
-const options = {
-  type: 'basic',
-  title: 'It is a time to blink',
-  message: 'Give your eyes a break and reduce your eye strain',
-  priority: 1,
-  // items: [
-  //   {
-  //     title: 'It is a time to blink',
-  //     message: 'Give your eyes a break and reduce your eye strain'
-  //   }
-  // ],
-  iconUrl: 'icon.png'
-};
+// const options = {
+//   type: 'basic',
+//   title: 'It is a time to blink',
+//   message: 'Give your eyes a break and reduce your eye strain',
+//   priority: 1,
+//   // items: [
+//   //   {
+//   //     title: 'It is a time to blink',
+//   //     message: 'Give your eyes a break and reduce your eye strain'
+//   //   }
+//   // ],
+//   iconUrl: 'icon.png'
+// };
 
-function sendNotification() {
-  // eslint-disable-next-line
-  chrome.notifications.create('alert' + new Date(), options, function(id) {
-    // eslint-disable-next-line
-    console.log('Last error:', id, chrome.runtime.lastError);
-  });
-}
+// function sendNotification() {
+//   // eslint-disable-next-line
+//   chrome.notifications.create('alert' + new Date(), options, function(id) {
+//     // eslint-disable-next-line
+//     console.log('Last error:', id, chrome.runtime.lastError);
+//   });
+// }
 
 // eslint-disable-next-line
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  setTimeout(() => {
-    sendNotification();
-  }, request.time);
+  console.log('onMessage-->', request);
+  if (request.durationTime) {
+    blinkerSettings.durationTime = request.durationTime;
+    // eslint-disable-next-line
+    chrome.storage.sync.set(blinkerSettings, function() {});
+  }
+  // eslint-disable-next-line
+  chrome.storage.sync.get(['durationTime'], function(items) {
+    console.log('durationTime after onMessage', items);
+  });
 });
--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 // eslint-disable-next-line
 /*global chrome*/
+import 'semantic-ui-css/semantic.min.css';
 
 import React from 'react';
+import { Header, Grid, Checkbox, Form, Icon } from 'semantic-ui-react';
 
 import './App.css';
 
 class App extends React.Component {
+  state = { durationTime: 1 };
+
+  handleChange = (e, { name, value }) => {
+    console.log(name, value);
+    this.setState({ [name]: value });
+
+    chrome.runtime.sendMessage({ [name]: value });
+  };
+
+  toggleVisibility = () =>
+    this.setState(prevState => ({ visible: !prevState.visible }));
+
   componentDidMount = () => {
-    // chrome.storage.sync.get(
-    //   ['durationTime', 'message', 'reminderDuration'],
-    //   function(items) {
-    //     console.log('durationTime', items);
+    // chrome.storage.sync.get(['durationTime'], items => {
+    //   if (items.durationTime) {
+    //     this.setState({
+    //       durationTime: items.durationTime / 1000
+    //     });
     //   }
-    // );
+    // });
   };
 
   handleClick = () => {
@@ -20,14 +35,45 @@ class App extends React.Component {
   };
 
   render() {
+    const { durationTime } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <p>F.C Barcelona</p>
+        <Grid columns={2} className="Item" textAlign="center">
+          <Grid.Column computer="8">
+            <Header as="h3">Eye Blinker</Header>
+          </Grid.Column>
+          <Grid.Column computer="8">
+            <Checkbox toggle onChange={this.handleClick} />
+          </Grid.Column>
+        </Grid>
 
-          {/* eslint-disable-next-line */}
-          <button onClick={this.handleClick}>Submit</button>
-        </header>
+        <Grid columns={1} className="Item">
+          <Grid.Column as={Form}>
+            <Form.Input
+              label={`Duration : ${durationTime} Minutes `}
+              min={1}
+              max={60}
+              name="durationTime"
+              onChange={this.handleChange}
+              step={1}
+              type="range"
+              value={durationTime}
+            />
+          </Grid.Column>
+        </Grid>
+
+        <Grid columns={2}>
+          <Grid.Column computer="8">
+            {/* <Header as="h5">Eye Blinker</Header> */}
+          </Grid.Column>
+
+          <Grid.Column computer="8" textAlign="right">
+            <Icon name="share alternate" size="large" />
+            <Icon name="twitter" size="large" />
+            <Icon name="facebook" size="large" />
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
